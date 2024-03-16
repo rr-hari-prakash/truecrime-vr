c11323c651c48890d7b905706940bbe5abe15e2d
Ivan Motta <ivanmotta@gmail.com>
2024-03-16 15:28:04 -0300

    passthrough and basic manager

---
+++ VR-Unity-Template-2023-main/Assets/_Scripts/MainManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public GameObject ObjectsMenu;
+
+
+    public void SwitchModes()
+    {
+
+    }
+
+    public void SelectedSomething(GameObject go)
+    {
+
+    }
+}

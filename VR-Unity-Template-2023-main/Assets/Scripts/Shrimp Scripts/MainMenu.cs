a4a5f2d9542b7e78621b094b6f9a5a670e1637f1
Ivan Motta <ivanmotta@gmail.com>
2024-03-16 00:17:21 -0300

    intro menu and interaction
    Basic menu, play and setup scenes. Interaction is working with grab only while overlapping object

--- VR-Unity-Template-2023-main/Assets/Scripts/Shrimp Scripts/MainMenu.cs
+++ VR-Unity-Template-2023-main/Assets/Scripts/Shrimp Scripts/MainMenu.cs
@@ -6,21 +6,30 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public string startButtonSceneName;
-    
+    public string setupButtonSceneName;
+
     public Button startButton;
+    public Button setupButton;
     public GameObject mainMenu;
     private void Awake()
     {
         startButton.onClick.AddListener(StartGame);
+        setupButton.onClick.AddListener(SetupGame);
     }
 
     private void OnDestroy()
     {
         startButton.onClick.RemoveListener(StartGame);
+        setupButton.onClick.RemoveListener(SetupGame);
     }
     private void StartGame()
     {
         mainMenu.SetActive(false);
         LevelManager.Instance.LoadSceneAsync(startButtonSceneName);
+    }
+    private void SetupGame()
+    {
+        mainMenu.SetActive(false);
+        LevelManager.Instance.LoadSceneAsync(setupButtonSceneName);
     }
 }
